a0c145c529bebcc7b35c8ae24b5c7db6e76f7c32
AlexTrish <sastrishin@yandex.ru>
2025-04-03 10:19:25 +0300

    gsg

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import inject from 'vite-plugin-inject';
+// import inject from 'vite-plugin-inject'; // Temporarily remove this plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    inject({
-			process: 'process/browser',
-		}),
+    // inject({
+    //   process: 'process/browser',
+    // }),
     react(),
   ],
   server: {
@@ -18,5 +18,4 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  
 });
